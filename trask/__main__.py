53421b239bb4d4257305c72d2058703041815d3c
Nicholas Bishop <nicholasbishop@gmail.com>
2018-12-26 20:22:50 -0500

    Rewrite main

--- trask/__main__.py
+++ trask/__main__.py
@@ -9,9 +9,11 @@ def main():
     parser.add_argument('path')
     args = parser.parse_args()
 
-    ctx = Context(args.path)
-
-    trask.run_trask_file(ctx, args.path)
+    ctx = trask.Context()
+    root = trask.load_trask_file(ctx, args.path)
+    root = trask.schema.SCHEMA.validate(root)
+    
+    trask.run(root)
 
 
 main()
