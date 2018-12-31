a002d86de64343154c672ac0f26736cda8c974a8
Nicholas Bishop <nicholasbishop@gmail.com>
2018-12-31 16:12:27 -0500

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@
 
 import setuptools
 
+
+def long_description():
+    with open('README.md') as rfile:
+        return f.read()
+
+
 setuptools.setup(
     name='trask',
     version='1.0.2',
     description='Declarative recipe-based task runner',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
     url='https://github.com/nicholasbishop/trask',
     author='Nicholas Bishop',
     author_email='nicholasbishop@gmail.com',
