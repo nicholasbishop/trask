e2b9fbcf205ec4eb6cf018431385ed6dfb52b81e
Nicholas Bishop <nicholasbishop@gmail.com>
2018-12-31 13:24:42 -0500

    Add test for __init__.py

--- tests/test_trask.py
+++ tests/test_trask.py
@@ -3,6 +3,9 @@
 import os
 import unittest
 
+from pyfakefs import fake_filesystem_unittest
+
+import trask
 from trask import functions
 
 
@@ -11,3 +14,12 @@ class TestFunctions(unittest.TestCase):
         os.environ['MY_TEST_VAR'] = 'my-test-value'
         self.assertEqual(
             functions.get_from_env(('MY_TEST_VAR', )), 'my-test-value')
+
+
+class TestInit(fake_filesystem_unittest.TestCase):
+    def setUp(self):
+        self.setUpPyfakefs()
+
+    def test_run(self):
+        self.fs.create_file('/myFile.trask')
+        trask.run('/myFile.trask', dry_run=True)
